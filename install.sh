7326b60e80d92b2c462e98290a4bc43ea7ecb747
Alberto Cubeddu <acubeddu87@gmail.com>
2015-04-03 13:29:24 +0100

    remove file

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PS3='Please enter your choice: I want to install: '
-options=("As Library" "As File" "Quit")
+options=("As Library" "Install file Locally" "Quit")
 
 select opt in "${options[@]}"
 do
@@ -10,12 +10,16 @@ do
             	cp ejaEditorLib.eja /opt/eja.it/lib/.  
                 break;
 		;;
-        "As File")
-            	echo -en "\nThe file will be copied in the current parent directory. You have to call it to execute it.\n"
-            	cd ..
-                cp ejaEditor/ejaEditorLib.eja .
-                break;
-		;;
+        "Install file Locally")
+                echo "\nThe file will be placed in this directory\n"
+                mkdir js
+                cd js
+                wget http://code.jquery.com/jquery-1.11.2.min.js
+                wget estoporaquello.com/ejaEditor/ace.tar.gz
+                tar -xvf ace.tar.gz 
+                rm ace.tar.gz
+        break;
+        ;;
         "Quit")
             break;
             ;;
